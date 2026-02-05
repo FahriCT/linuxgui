034d0b9761e5c45d59dad0d58ce33c406c6b7ec0
Fahri Maulana Rohandi <84897844+FahriCT@users.noreply.github.com>
2026-02-06 00:39:45 +0700

    Add start script for XRDP session management

---
+++ test/start.sh
@@ -0,0 +1,14 @@
+xrdp-sesman </dev/null >/dev/null 2>&1 &
+xrdp -nodaemon </dev/null >/dev/null 2>&1 &
+echo "Can open Google Chrome by execute \" ggcr \" on remote terminal"
+echo "XRDP run at port 3389"
+echo -ne """
+Credentials:
+User: $1
+Pass: $2
+
+Root Credentials:
+User: root
+Pass: $3
+"""
+sleep 9999999d
